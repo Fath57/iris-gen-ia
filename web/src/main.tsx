76814e0ae18f14042b537d33cec995fda94552f1
Yanis-Bouzidi <yanisbouzidi00@gmail.com>
2026-04-30 12:44:08 +0200

    add tailwind - shadcn - router

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,24 @@
+// src/main.tsx (ou main.jsx)
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App'
+import Test from './pages/Test'
+
+const router = createBrowserRouter([
+  {
+    path: "/", 
+    element: <App />,
+  },
+  {
+    path: "/test",
+    element: <Test />,
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 )
---
+++ web/src/pages/Test.tsx
@@ -0,0 +1,8 @@
+export default function Test() {
+  return (
+    <div className="p-12 bg-red-500">
+      <h1>Test</h1>
+      <p>Ceci est une page de test pour le routeur !</p>
+    </div>
+  )
+}
